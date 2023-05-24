1c31583abf500c10f3628bef238ec8133d9a5a98
Brandon Ruffridge <bruffridge@users.noreply.github.com>
2023-05-24 15:52:38 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,9 +41,9 @@ class ChatBot(discord.Client):
                            "Evaluate a given a design challenge based on how well it adheres to the criteria below. If the design challenge is too broad or narrow, suggest improvements or alternatives.\n\n"
                            "Criteria: The first step in any design process is to define the problem or opportunity that you want your design to address. Clearly articulate the impact you want your design to have in the world and the criteria and constraints that will determine success. The goal of this step is not to decide what you will make or design but to understand what your design needs to do, for whom, and in what context. Once you have an idea of what you want to work on, try stating your challenge in one sentence. In order to avoid jumping to conclusions about what you will design, try stating the challenge as a question that begins with, “How might we…?”. Make sure you are considering context. Context provides specificity and constraints with which to work. Context can include many factors, but it’s essential to identify the needs of your stakeholders (those who will be impacted) and the location or setting in which your design will be used. Without this context a design challenge is often too broad to be addressed successfully. On the other hand, be careful not to define the context too narrowly. Applying too many constraints before beginning the design process can limit the number and variety of potential solutions. Asking the right question at the beginning of your project will guide you in your research and give you a better chance of arriving at an innovative and impactful solution. Take a systems view and look for potential leverage points. Think about the system surrounding the problem (or opportunity) you are designing for. What interactions and relationships are part of its context? What are the system boundaries and connections to other systems? Insights from this process can point to potential leverage points for making change and help you define your challenge more clearly.")
         self.instructions = "".join(["Welcome to BIDARA, a Bio-Inspired Design and Research Assistant AI chatbot that uses OpenAI’s GPT-4 model to respond to queries.\n",
-                                     "As you chat back and forth either through DMs or in #chat-with-bidara, BIDARA keeps track of all the messages between you and it as part of your unique conversation history. ",
+                                     "As you chat back and forth either through private messages or in #chat-with-bidara, BIDARA keeps track of all the messages between you and it as part of your unique conversation history. ",
                                      "This allows it to respond to new queries based on the context of your conversation. Eventually your conversation will need to be cleared or OpenAI will not be able to generate new responses. ",
-                                     "To clear the conversation, and start a new one, use the `!clear_conv` command.\n\n",
+                                     "To clear the conversation, and start a new one, use the `!clearconv` command.\n\n",
                                      "BIDARA can be directed to respond in certain ways, by using GPT-4’s system prompt. By default the system prompt BIDARA uses allows it to respond in ways helpful for bio-inspired design and research assistant activities. ",
                                      "Please let us know about your experiences, good or bad, in the #bidara-feedback channel so we can improve it.\n\n",
                                      "**Do not share any sensitive information** in your conversations including but not limited to, personal information, sensitive or private government/company data, ITAR, CUI, export controlled, or trade secrets.\n",
@@ -52,14 +52,14 @@ class ChatBot(discord.Client):
                                      "Here are the in-built commands:\n",
                                      "`!help` - description of bot and commands.\n",
                                      "`!examples` - show examples of what BIDARA can do.\n",
-                                     "`!system` - lists your current system prompt.\n",
-                                     "`!set_default_sys` - set your system prompt to the default BIDARA prompt.\n",
-                                     "`!set_define_sys` - set your system prompt to one that instructs BIDARA to evaluate a given design challenge and offer suggested improvements using suggestions from the Design step from Biomimicry Institute's Biomimicry Design Process.\n",
-                                     "`!set_bda_sys` - set your system prompt to one that instructs BIDARA to perform Biologize, Discover, and Abstract steps from Biomimicry Institute's Biomimicry Design Process on a given design challenge question.\n",
-                                     "`!set_custom_sys` - set a custom system prompt.\n",
-                                     "`!clear_sys` - clear your current system prompt.\n",
-                                     "`!curr_conv` - shows your current conversation.\n",
-                                     "`!clear_conv` - clear your current conversation.\n",
+                                     "`!mode` - lists your current system prompt.\n",
+                                     "`!defaultmode` - set your system prompt to the default BIDARA prompt.\n",
+                                     "`!definemode` - set your system prompt to one that instructs BIDARA to evaluate a given design challenge and offer suggested improvements using suggestions from the Design step from Biomimicry Institute's Biomimicry Design Process.\n",
+                                     "`!bdamode` - set your system prompt to one that instructs BIDARA to perform Biologize, Discover, and Abstract steps from Biomimicry Institute's Biomimicry Design Process on a given design challenge question.\n",
+                                     "`!custommode` - set a custom system prompt.\n",
+                                     "`!clearmode` - clear your current system prompt.\n",
+                                     "`!conv` - shows your current conversation.\n",
+                                     "`!clearconv` - clear your current conversation.\n",
                                      "\n\n"])
 
         self.examples = "".join(["**Bio-inspired non-toxic white paint**\n",
@@ -133,17 +133,17 @@ class ChatBot(discord.Client):
         if prompt_choice == "default":
             self.system_prompt_dict[message.author] = self.default_sys
             await message.channel.send(f"Your system prompt is set to:\n>>> {self.default_sys}\n\n")
-            await message.channel.send("If you would like to change or clear it, type `!set_custom_sys` or `!clear_sys`, respectively.")
+            await message.channel.send("If you would like to change or clear it, type `!custommode` or `!clearmode`, respectively.")
         elif prompt_choice == "bda":
             self.system_prompt_dict[message.author] = self.bda_sys
             await self.send_msg("Your system prompt is set to:\n", message)
             await self.send_msg(f"{self.bda_sys}\n\n", message, prefix=">>> ")
-            await self.send_msg("If you would like to change or clear it, type `!set_custom_sys` or `!clear_sys`, respectively.", message)
+            await self.send_msg("If you would like to change or clear it, type `!custommode` or `!clearmode`, respectively.", message)
         elif prompt_choice == "define":
             self.system_prompt_dict[message.author] = self.define_sys
             await self.send_msg("Your system prompt is set to:\n", message)
             await self.send_msg(f"{self.define_sys}\n\n", message, prefix=">>> ")
-            await self.send_msg("If you would like to change or clear it, type `!set_custom_sys` or `!clear_sys`, respectively.", message)
+            await self.send_msg("If you would like to change or clear it, type `!custommode` or `!clearmode`, respectively.", message)
         elif prompt_choice == "custom":
             self.custom_sys = True
 
@@ -176,17 +176,18 @@ class ChatBot(discord.Client):
     async def process_keyword(self, keyword, message):
         if keyword == "help":
             await self.send_msg(self.instructions, message)
-        elif keyword == "system":
+        elif keyword == "mode":
             await self.process_system_prompt(message)
-        elif keyword[-3:] == "sys" and keyword[:3] == "set":
-            prompt_choice = keyword.split("_")[1]
-            await self.set_system_prompt(prompt_choice, message)
-        elif keyword == "clear_sys":
+        elif keyword == "clearmode":
             self.system_prompt_dict[message.author] = ""
-            await message.channel.send("Your system prompt for Chat-GPT is cleared.")
-        elif keyword == "curr_conv":
+            await message.channel.send("Your system prompt is cleared.")
+        elif keyword[-4:] == "mode":
+            prompt_choice = keyword[:-4]
+            await self.set_system_prompt(prompt_choice, message)
+        
+        elif keyword == "conv":
             await self.list_conv(message)
-        elif keyword == "clear_conv":
+        elif keyword == "clearconv":
             if message.author in self.conversations:
                 self.system_prompt_dict[message.author] = ""
                 self.conversations[message.author] = []
@@ -221,7 +222,7 @@ class ChatBot(discord.Client):
             try:
                 response = await self.call_openai(self.conversations[message.author])
             except:
-                await message.channel.send("OpenAI experienced an error generating a response. Maybe your conversation has grown too large, try `!clear_conv` to clear it, then try again. Or OpenAI may be currently overloaded with other requests. You can retry again after a short wait.")
+                await message.channel.send("OpenAI experienced an error generating a response. Maybe your conversation has grown too large, try `!clearconv` to clear it, then try again. Or OpenAI may be currently overloaded with other requests. You can retry again after a short wait.")
             else:
                 assistant_response = response['choices'][0]['message']['content']
 
